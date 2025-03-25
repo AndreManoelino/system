d051b134b419b0a2dfd6500c9acc7636233c1b20
AndreManoelino <139048756+AndreManoelino@users.noreply.github.com>
2025-03-25 15:34:18 -0300

    Add files via upload

--- curso java/os/src/main/java/com/andre/os/OsApplication.java
+++ curso java/os/src/main/java/com/andre/os/OsApplication.java
@@ -3,10 +3,17 @@ package com.andre.os;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class OsApplication {
+
+
     public static void main(String[] args) {
-        System.setProperty("server.port", System.getenv("PORT")); // Ajusta a porta no Heroku
         SpringApplication.run(OsApplication.class, args);
+
+
     }
+
+
 }
--- curso java/os/src/main/java/com/andre/os/dtos/ClienteDTO.java
+++ curso java/os/src/main/java/com/andre/os/dtos/ClienteDTO.java
@@ -11,7 +11,7 @@ import java.io.Serializable;
 
 public class ClienteDTO extends TecnicoDTO implements Serializable {
     @Serial
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L; // Cetando alterações
 
     private Integer id;
 
